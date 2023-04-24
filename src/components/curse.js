9fb7b0d71e4e19f367025a6487d95d19afc08970
aaronjtonner <tonneraaron@gmail.com>
2023-04-24 02:32:13 -0600

    fix

--- src/components/curse.js
+++ src/components/curse.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Flex, Section } from "../layoutComponents"
+import { Container, Flex, Section } from "./layoutComponents"
 import styled from "styled-components"
 
 const Wrapper = styled.div`
